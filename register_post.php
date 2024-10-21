c09cc8a319ad3cd8be72f6170dfc17cb0b35fbda
tarikaziz0933 <tarikaziz0933@gmail.com>
2024-10-21 10:21:34 +0600

    added single registration

--- register_post.php
+++ register_post.php
@@ -5,9 +5,21 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-$insert = "INSERT INTO users(name,email,password) values('$name', '$email', '$password')";
-$insert_result = mysqli_query($db_connect, $insert);
-$_SESSION['success'] = 'User added sucessfully';
-header('location:register.php');
+$select = "SELECT COUNT(*) as gotit FROM users WHERE email='$email'";
+$select_result = mysqli_query($db_connect, $select);
+$after_assoc = mysqli_fetch_assoc($select_result);
+
+if($after_assoc['gotit'] == 1){
+    echo 'already exit';
+}
+else{
+    $insert = "INSERT INTO users(name,email,password) values('$name', '$email', '$password')";
+    $insert_result = mysqli_query($db_connect, $insert);
+    $_SESSION['success'] = 'User added sucessfully';
+    header('location:register.php');
+}
+
+
+
 
 ?>
--- update_user.php
+++ update_user.php
@@ -13,15 +13,11 @@ if(empty($password)){
     $update_user = "UPDATE users SET name='$name', email='$email' WHERE id=$id";
     $update_user_result = mysqli_query($db_connect, $update_user);
     $_SESSION['update'] = 'User Updated';
-    // print_r( $_SESSION['update']);
-    // die();
     header('location:users.php');
 }else{
     $update_user = "UPDATE users SET name='$name', email='$email', password='$password_hash' WHERE id=$id";
     $update_user_result = mysqli_query($db_connect, $update_user);
     $_SESSION['update'] = 'User Updated!';
-    // echo $_SESSION['update'];
-    // die();
     header('location:users.php');
 }
 
